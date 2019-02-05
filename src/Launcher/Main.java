292df87b7f0e1d8d8c95e4f59b250bd0bcaf9089
oussamabonnor1 <oussamabonnor@gmail.com>
2019-02-05 14:45:26 +0100

    Creating views and controllers (empty)

--- src/Launcher/Main.java
+++ src/Launcher/Main.java
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../Views/sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/viewStock.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 760));
         primaryStage.show();
     }
 
