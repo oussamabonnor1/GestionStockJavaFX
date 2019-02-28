425ec593891735b5a0018b9f134fef0067b9ad75
oussamabonnor1 <oussamabonnor@gmail.com>
2019-02-28 15:20:04 +0100

    fixing views

--- src/Controllers/ControllerReceipt.java
+++ src/Controllers/ControllerReceipt.java
@@ -1,10 +1,16 @@
 package Controllers;
 
+import Launcher.Launcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class ControllerReceipt {
 
@@ -12,40 +18,34 @@ public class ControllerReceipt {
     private TableView<?> tableClient;
 
     @FXML
-    private TableColumn<?, ?> colNBon;
-
-    @FXML
-    private TableColumn<?, ?> colDate;
-
-    @FXML
-    private TableColumn<?, ?> colNFournisseur;
+    private TableColumn<?, ?> colNBon, colDate, colNFournisseur;
 
     @FXML
     private TableView<?> tableFournisseur;
 
     @FXML
-    private TableColumn<?, ?> colFournisseurNum;
-
-    @FXML
-    private TableColumn<?, ?> colNameFournisseur;
-
-    @FXML
-    private TableColumn<?, ?> colAdresseFournisseur;
-
-    @FXML
-    private TableColumn<?, ?> colTelephoneFournisseur;
-
-    @FXML
-    private TableColumn<?, ?> colFaxFournisseur;
+    private TableColumn<?, ?> colFournisseurNum, colNameFournisseur, colAdresseFournisseur, colTelephoneFournisseur, colFaxFournisseur;
 
     @FXML
     void articleViewSelected(MouseEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Views/viewArticle.fxml"));
+            Scene scene = new Scene(root, Launcher.stage.getScene().getWidth(), Launcher.stage.getScene().getHeight());
+            Launcher.stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     void stockViewSelected(MouseEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Views/viewStock.fxml"));
+            Scene scene = new Scene(root, Launcher.stage.getScene().getWidth(), Launcher.stage.getScene().getHeight());
+            Launcher.stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -58,4 +58,13 @@ public class ControllerReceipt {
 
     }
 
+    public void clientViewSelected(MouseEvent mouseEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Views/viewPersonel.fxml"));
+            Scene scene = new Scene(root, Launcher.stage.getScene().getWidth(), Launcher.stage.getScene().getHeight());
+            Launcher.stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Launcher/Launcher.java
+++ src/Launcher/Launcher.java
@@ -5,15 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Launcher extends Application {
     public static Stage stage;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../Views/viewArticle.fxml"));
         stage = primaryStage;
         stage.setTitle("Stock management");
         stage.setScene(new Scene(root, 1360, 680));
+        //stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
